2bb487653ed1a40e5733e198bc8c5c70060ea040
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-18 15:08:45 +0430

    building the center of the header

--- components/Header.js
+++ components/Header.js
@@ -14,18 +14,31 @@ import {
   SearchIcon,
   ShoppingCartIcon
 } from '@heroicons/react/outline'
+import HeaderIcon from './HeaderIcon'
 
 function Header() {
   return (
-    <div>
+    <div className='sticky top-0 z-50 bg-white flex items-center lg:px-5 shadow-md'>
       <div className='flex items-center'>
         <Image src='https://links.papareact.com/5me'
           width={40}
           height={40}
-          layout="fixed" />
+          layout="fixed"
+          alt='fb-logo' />
         <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
           <SearchIcon className='h-6 text-gray-600' />
-          <input className='flex ml-1 items-center bg-transparent outline-none placeholder-gray-500' type='text' placeholder='Search Facebook' />
+          <input className='flex ml-1 items-center bg-transparent 
+          outline-none placeholder-gray-500 flex-shrink' type='text' placeholder='Search Facebook' />
+        </div>
+      </div>
+
+      <div className='flex justify-center grow'>
+        <div className='flex space-x-6 md:space-x-2'>
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
         </div>
       </div>
     </div>
