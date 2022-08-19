017a0014c1dbc2f7dbac62a7559ba5eeca00357a
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-19 14:40:13 +0430

    building the login component

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,10 @@ import Auth0Provider from "next-auth/providers/auth0"
 function MyApp({ Component, pageProps }) {
   return (
 
-    <Auth0Provider session={pageProps.session}>
+    // <Auth0Provider session={pageProps.session}>
       
       <Component {...pageProps} />
-    </Auth0Provider>
+    // </Auth0Provider>
   )
 }
 
