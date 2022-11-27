bfb5c62ba2e13fafd9e68e073a746a711dd30b3d
Arash Hosseini <arashhosseini733@gmail.com>
2022-11-27 11:09:26 +0330

    Building the Feed and Stories Components

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import { getSession } from "next-auth/react"
 import Login from '../pages/auth/signin'
 import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 export default function Home({session}) {
   return (
@@ -15,6 +16,7 @@ export default function Home({session}) {
 
       <main className='flex'>
         <Sidebar />
+        <Feed />
       </main>
     </div>
   )
